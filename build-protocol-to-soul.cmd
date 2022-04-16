9a350470dbb4bab87ab61be46f12c9fed8310cb9
jc <jiowchern@gmail.com>
2022-04-16 09:26:03 +0800

    add ghost protocol build.

--- build-protocol-to-soul.cmd
+++
@@ -1,1 +0,0 @@
-dotnet build -o ./Game/Astringent.Game20220410.Soul/Assets/Project/Protocol --no-dependencies  ./Base/Astringent.Game20220410.Protocol/
---
+++ build-protocol.cmd
@@ -0,0 +1,2 @@
+dotnet build -o ./Game/Astringent.Game20220410.Soul/Assets/Project/Library/Protocol --no-dependencies  ./Base/Astringent.Game20220410.Protocol/
+dotnet build -o ./Game/Astringent.Game20220410.Ghost/Assets/Project/Library/Protocol --no-dependencies  ./Base/Astringent.Game20220410.Protocol/
